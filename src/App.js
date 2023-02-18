ae26562385b4680301d92bacff346e4815c6f2e5
mdshahnawaz786 <mdshahnawaz911@gmail.com>
2023-02-18 19:06:40 +0530

    secongAss

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1 class="text">The Evolution of</h1>
+      <h1 class="text">Airbnb's Frontend</h1>
+      <h5 class="text">@spikebrehm</h5>
+      <img class="image" src='https://images.opencollective.com/airbnb/d327d66/logo.png'/>
+      <h6 class="text-last">airbnb</h6>
     </div>
   );
 }
